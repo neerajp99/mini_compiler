2c3be533e7ca9f3bd1a9339f14921746f27e6a46
neerajp99 <neerajp1999@gmail.com>
2021-12-12 11:54:06 +0530

    Update parser for unary operator

--- src/parser.py
+++ src/parser.py
@@ -1,5 +1,7 @@
 from src import lexer
 # Create a parser to parse the lexer output
+
+
 class Parser:
     def __init__(self, lexer):
         self.lexer = lexer
@@ -12,19 +14,30 @@ class Parser:
         """Method to utilise the current token and assign the next token to the current token"""
         if self.current_token.token_type == token_type:
             self.current_token = self.lexer.get_next_token()
+            print(self.current_token)
         else:
             self.error()
 
     def factor(self):
         """Method to parse the factor"""
         token = self.current_token
-        if token.token_type == 'INTEGER' or token.token_type == 'FLOAT':
+        if token.token_type == 'LPAREN':
+            self._utilise('LPAREN')
+            paren_node = self.expr()
+            self._utilise('RPAREN')
+            return paren_node
+        if token.token_type == 'INTEGER':
             self._utilise('INTEGER')
             return Number(token)
-        elif token.token_type == 'LPAREN':
-            self._utilise('LPAREN')
-            self._utilise('RPAREN')
-            return self.expr()
+        elif token.token_type == 'FLOAT':
+            self._utilise('FLOAT')
+            return Number(token)
+        elif token.token_type == 'MINUS':
+            self._utilise('MINUS')
+            return UnaryOperator(
+                op_token=token,
+                right=self.factor()
+            )
         else:
             self.error()
 
@@ -39,8 +52,8 @@ class Parser:
                 self._utilise('TT_DIV')
 
             left_node = BinOperator(
-                left=left_node, 
-                op_token=token, 
+                left=left_node,
+                op_token=token,
                 right=self.factor()
             )
 
@@ -58,12 +71,13 @@ class Parser:
                 self._utilise('MINUS')
 
             left_node = BinOperator(
-                left=left_node, 
-                op_token=token, 
+                left=left_node,
+                op_token=token,
                 right=self.term()
             )
 
         return left_node
+
 
 class Number:
     def __init__(self, token):
@@ -72,15 +86,26 @@ class Number:
     def __str__(self):
         return f'{self.value}'
 
+
 class BinOperator:
     def __init__(self, left, op_token, right):
         self.left = left
         self.op_token = op_token
         self.right = right
-    
+
     def __str__(self):
         return f'({self.left}, {self.op_token}, {self.right})'
 
-x = Parser(lexer.Lexer('2*3+5*7'))
+
+class UnaryOperator:
+    def __init__(self, op_token, right):
+        self.op_token = op_token
+        self.right = right
+
+    def __str__(self):
+        return f'({self.op_token}, {self.right})'
+
+
+x = Parser(lexer.Lexer('-2.8 + 3 * (10 + 10)'))
 y = x.expr()
 print(y)
